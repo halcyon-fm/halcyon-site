63681cdb772a148cdc6279f01fd74e11e28e6c3b
zyshara <hi.imma.bear@gmail.com>
2021-05-24 01:27:29 -0700

    ddd ep

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -13,6 +13,9 @@ function Release(name, path, link) {
 }
 
 const RELEASES = [
+const RELEASES = [
+  new Release("DDD - aetherplane", "aetherplane.png", "https://go.halcyon.fm/aether_plane"),
+  new Release("DDD - glassheart", "glassheart.png", "https://go.halcyon.fm/glass_heart"),
   new Release("I7 - SEVEN", "seven.png", "https://go.halcyon.fm/seven"),
   new Release("I7 - BORED", "bored.png", "https://go.halcyon.fm/bored"),
   new Release("I7 - VII", "30.png", "https://go.halcyon.fm/vii"),
